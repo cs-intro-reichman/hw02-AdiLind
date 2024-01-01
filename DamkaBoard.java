0bbd099c23f5dd55ec938291e0d14d33924a6601
AdiLind <57391741+AdiLind@users.noreply.github.com>
2024-01-01 23:23:27 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,18 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		for(int i = 0; i < n ; i++) //שורה
+		{
+			if ( i % 2 != 0) // if the row is even you should print 1 space inside
+			{
+				System.out.print(" ");
+			}
+			for(int j=0 ; j < n ; j++) // עמודות
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
 	}
 }
