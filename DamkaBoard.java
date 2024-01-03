84eaaa9f764be1de82d25ef96aef395a4df7cbd1
AdiLind <57391741+AdiLind@users.noreply.github.com>
2024-01-03 22:07:07 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,14 +4,17 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 	int n = Integer.parseInt(args[0]);
+
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0 && i > 0) { 
-                System.out.print(" ");
+            if (i % 2 == 0 && i > 0) {
+                System.out.print(" "); 
             }
+
             for (int j = 0; j < n; j++) {
                 System.out.print("* "); 
             }
-            System.out.println();
+
+            System.out.println(); 
         }
 	}
 }
