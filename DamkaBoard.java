a2295f6d32c64b2c6be1d42a3ae77fbefb6c8391
AdiLind <57391741+AdiLind@users.noreply.github.com>
2024-01-03 22:17:19 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,18 +5,16 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 	int n = Integer.parseInt(args[0]);
 
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) { // Even row: Print space before asterisks
+       for (int i = 0; i < n; i++) {
+            if (i % 2 == 0 && i > 0) {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j < n; j++) {
-                System.out.print("*"); // Print asterisk
-                if (i % 2 != 0) { // Odd row: Print space after asterisk
-                    System.out.print(" ");
-                }
+            for (int j = 0; j < n - 1; j++) { 
+                System.out.print("* ");
             }
 
+            System.out.print("*"); 
             System.out.println(); 
         }
 	}
